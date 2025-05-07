06db447f44f133ab8d99a6f9bc53727c18ca654c
Роман Бубнов <romanbubnov@MacBook-Pro-Roman.local>
2025-05-07 20:38:43 +0400

    test: testing work pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import type { ReactElement } from "react";
+
 import "./App.css";
 
-function App() {
+function App():ReactElement {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -26,7 +24,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more 
       </p>
     </>
   );
