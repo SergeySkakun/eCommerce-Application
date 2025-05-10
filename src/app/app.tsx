c6b676bb6350c32ab79b0dd7ac74556f45f949c6
Dmitry <Dololob92@gmail.com>
2025-05-10 19:41:36 +0300

    feat: obtain acces token

---
+++ src/app/app.tsx
@@ -0,0 +1,4 @@
+import { ObtainAccessToken } from "./../shared/api/obtain-access-token";
+import { createRoot } from "react-dom/client";
+const root = createRoot(document.querySelector("#root"));
+root.render(<ObtainAccessToken />);
