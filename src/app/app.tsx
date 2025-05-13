5c0c4cdf88909a540fddf8c49e55e643859303c0
Dmitry <Dololob92@gmail.com>
2025-05-13 14:43:56 +0300

    fix: changed variable names in the env file

--- src/app/app.tsx
+++
@@ -1,4 +0,0 @@
-import { ObtainAccessToken } from "./../shared/api/obtain-access-token";
-import { createRoot } from "react-dom/client";
-const root = createRoot(document.querySelector("#root"));
-root.render(<ObtainAccessToken />);
