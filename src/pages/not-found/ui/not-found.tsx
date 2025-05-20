92b67321f6c9468d7bd225b094148992e90479a6
Sergey Skakun <sergeylearn116@gmail.com>
2025-05-20 17:05:46 +0300

    fix: change images pathes in 404 page

--- src/pages/not-found/ui/not-found.tsx
+++ src/pages/not-found/ui/not-found.tsx
@@ -6,9 +6,9 @@ import "./style.css";
 export function NotFound(): ReactElement {
   return (
     <div className="page-404">
-      <img className="right-car car-1" src="/assets/cars-404/1.jpg"></img>
+      <img className="right-car car-1" src="assets/cars-404/1.jpg"></img>
       <p className="oops-text">Oops...</p>
-      <img className="left-car car-2" src="/assets/cars-404/2.jpg"></img>
+      <img className="left-car car-2" src="assets/cars-404/2.jpg"></img>
       <img className="right-car car-3" src="assets/cars-404/3.jpg"></img>
       <p className="error-text">404 Not Found</p>
       <Link to="/main">
@@ -16,11 +16,11 @@ export function NotFound(): ReactElement {
           Back to Main page
         </Button>
       </Link>
-      <img className="left-car car-4" src="/assets/cars-404/4.jpg"></img>
-      <img className="right-car car-5" src="/assets/cars-404/5.jpg"></img>
-      <img className="left-car car-6" src="/assets/cars-404/6.jpg"></img>
-      <img className="right-car car-7" src="/assets/cars-404/7.jpg"></img>
-      <img className="left-car car-8" src="/assets/cars-404/8.jpg"></img>
+      <img className="left-car car-4" src="assets/cars-404/4.jpg"></img>
+      <img className="right-car car-5" src="assets/cars-404/5.jpg"></img>
+      <img className="left-car car-6" src="assets/cars-404/6.jpg"></img>
+      <img className="right-car car-7" src="assets/cars-404/7.jpg"></img>
+      <img className="left-car car-8" src="assets/cars-404/8.jpg"></img>
     </div>
   );
 }
