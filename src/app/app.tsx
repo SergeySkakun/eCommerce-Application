01184f0894a4ad1e4c1fef944b8dec1701e5d880
Dmitry <Dololob92@gmail.com>
2025-05-21 14:01:25 +0300

    fix: request for an anonymous token

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,10 @@
 import type { ReactNode } from "react";
 import { PageRouter } from "./routing";
 import { AuthUserContextProvider } from "./routing";
-import { ObtainAnonymousAccessToken } from "../shared/api";
 
 export function App(): ReactNode {
   return (
     <AuthUserContextProvider>
-      <ObtainAnonymousAccessToken />
       <PageRouter />
     </AuthUserContextProvider>
   );
