4d1d07f5826d6a43cfcc60ecd4a8f201d5ecd5cd
Dmitry <Dololob92@gmail.com>
2025-05-27 14:43:01 +0300

    feat: added sorting option

--- src/pages/catalog-product/api/index.ts
+++ src/pages/catalog-product/api/index.ts
@@ -1,3 +1,3 @@
-export { sendingFilterRequest } from "./filter-request";
+export { sendingFilterSortingRequest } from "./filter-sorting-request";
 export { getAllProducts } from "./request-to-get-all-products";
 export * from "./types";
