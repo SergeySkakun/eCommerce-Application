3817d0d994adc304811d7673adf31ffeb6ccd038
Dmitry <Dololob92@gmail.com>
2025-06-03 17:19:46 +0300

    feat: added card output to the home page and a back button on the detailed product page

--- src/pages/detailed-product/ui/detailed-product.tsx
+++ src/pages/detailed-product/ui/detailed-product.tsx
@@ -5,6 +5,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ProductContent } from "./product-content";
 import { getProduct } from "../api";
 import { LoadingPlaceholder, type MasterData } from "../../../shared";
+import "./styles.css";
 
 const darkTheme = createTheme({
   palette: {
