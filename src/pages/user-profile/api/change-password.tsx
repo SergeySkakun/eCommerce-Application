c2f4f59734510f356b2a0cfd900d30ff9661ddf5
Dima <113242492+DmitryStarchenko@users.noreply.github.com>
2025-06-03 14:42:30 +0300

    feat: added receiving token after password change

--- src/pages/user-profile/api/change-password.tsx
+++ src/pages/user-profile/api/change-password.tsx
@@ -1,10 +1,16 @@
-import { API_HOST, PROJECT_KEY } from "../../../project-config";
+import {
+  API_HOST,
+  AUTH_HOST,
+  CLIENT_ID,
+  CLIENT_SECRET,
+  PROJECT_KEY,
+} from "../../../project-config";
 import {
   getTokenFromCookie,
   saveTokenCookie,
   TOKEN_NAMES,
 } from "../../../shared";
-import type { Customer } from "../../../shared";
+import type { AccessToken, Customer } from "../../../shared";
 
 // Изменение пароля:
 // передаём в функцию, введённый пользователем старый и новый пароль
@@ -30,10 +36,25 @@ export async function changePassword(
     body: JSON.stringify(body),
   })
     .then((response) => response.json())
-    .then((data: Customer) => {
+    .then(async (data: Customer) => {
       if (data.id) {
         message = "Password successfully changed";
         saveTokenCookie(data.version.toString(), TOKEN_NAMES.userVersion);
+        await fetch(
+          `${AUTH_HOST}/oauth/${PROJECT_KEY}/customers/token?grant_type=password&username=${data.email}&password=${body.newPassword}`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Basic ${btoa(`${CLIENT_ID}:${CLIENT_SECRET}`)}`,
+            },
+          },
+        )
+          .then((response) => response.json())
+          .then((data: AccessToken) => {
+            saveTokenCookie(data.access_token, TOKEN_NAMES.successUserAccess);
+            saveTokenCookie(data.refresh_token, TOKEN_NAMES.successUserRefresh);
+          })
+          .catch(() => console.error("No connection"));
       } else {
         message = data.message;
       }
