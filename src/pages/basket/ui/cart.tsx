ef3483142888f4e5d9a8ec9f39f074743e5a2c5b
Dmitry <Dololob92@gmail.com>
2025-06-06 22:12:28 +0300

    feat: getting a cart when click on the button

--- src/pages/basket/ui/cart.tsx
+++ src/pages/basket/ui/cart.tsx
@@ -1,7 +1,11 @@
-import type { ReactElement } from "react";
+import { type ReactElement } from "react";
 import "./styles.css";
+import type { Cart } from "../../../shared";
+import { getCart } from "../api";
 
-export function Cart(): ReactElement {
+export async function Cart(): Promise<ReactElement> {
+  const dataCart = await getCart();
+  console.log(dataCart);
   return (
     <main className="main-cart">
       <div className="cart-content">Cart</div>
