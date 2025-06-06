d70349bc12d08e65d459c8b066765e141d296c42
Dmitry <Dololob92@gmail.com>
2025-06-06 21:44:50 +0300

    feat: added icon cart

--- src/pages/basket/ui/cart.tsx
+++ src/pages/basket/ui/cart.tsx
@@ -1,15 +1,8 @@
 import { type ReactElement } from "react";
 import "./styles.css";
 import type { Cart } from "../../../shared";
-import { getCart } from "../api";
-
-const getCurrentCart = async (): Promise<void> => {
-  await getCart();
-};
 
 export function Cart(): ReactElement {
-  void getCurrentCart();
-
   return (
     <main className="main-cart">
       <div className="cart-content">Cart</div>
