6c8ac3405c1f03f676c112f11f6789641250b2b6
Роман Бубнов <romanbubnov@MacBook-Pro-Roman.local>
2025-06-07 16:43:05 +0400

    feat: add button to catalog page

--- src/pages/basket/ui/components/empty-cart.tsx
+++ src/pages/basket/ui/components/empty-cart.tsx
@@ -1,6 +1,7 @@
 import { Box, IconButton, Paper, Tooltip, Typography } from "@mui/material";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { type ReactElement } from "react";
+import { Link } from "react-router-dom";
 
 export function EmptyCart(): ReactElement {
   return (
@@ -27,16 +28,26 @@ export function EmptyCart(): ReactElement {
         }}
       >
         <Tooltip title="Go to Catalog" placement="top">
-          <IconButton aria-label="go to catalog">
-            <AddShoppingCartIcon sx={{ margin: 2 }} />
-          </IconButton>
+          <Link to="/catalog">
+            <IconButton aria-label="go to catalog">
+              <AddShoppingCartIcon sx={{ margin: 2 }} />
+            </IconButton>
+          </Link>
         </Tooltip>
-        <Typography gutterBottom variant="button" sx={{ mt: 2 }}>
-          Your Cart is currently Empty
-        </Typography>
-        <Typography color="textSecondary" gutterBottom variant="body2">
-          Is your journey to your perfect car just beginning?{" "}
-        </Typography>
+        <Box sx={{ my: 4 }}>
+          <Typography gutterBottom variant="button" sx={{ mt: 2 }}>
+            Your Cart is currently Empty
+          </Typography>
+          <Typography color="textSecondary" gutterBottom variant="body2">
+            Is your journey to your perfect car just beginning?{" "}
+          </Typography>
+        </Box>
+        <Link to="/catalog" className="link">
+          <button className="button-main-head">
+            CHOOSE THE RIGHT COMPANION
+          </button>
+        </Link>
+        {/* <ViewProducts /> */}
       </Paper>
     </Box>
   );
