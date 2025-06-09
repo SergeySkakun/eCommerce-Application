7431967350e6d7dc50cb28979fc780199be818dc
Dmitry <Dololob92@gmail.com>
2025-06-10 00:01:14 +0300

    feat: remove product from cart functionality

--- src/shared/ui/button-add-to-cart/button-add-to-cart.tsx
+++ src/shared/ui/button-add-to-cart/button-add-to-cart.tsx
@@ -10,31 +10,45 @@ export function ButtonAddToCart({
   productId: string;
 }): ReactElement {
   const [isProductInCart, setIsProductInCart] = useState(false);
+  const [lineItemId, setLineItemId] = useState<string>();
+
   const { setTotalLineItemQuantity } = useContext(TotalLineItemQuantityContext);
-
-  const actions = {
-    action: "addLineItem",
-    productId: productId,
-  };
-
   useEffect(() => {
     const productCheckout = async (): Promise<void> => {
       const cart = await getCart();
       const productInCart = cart.lineItems;
       if (productInCart.length > 0) {
-        const hasProductInCart = productInCart.some(
-          (product) => product.productId === productId,
-        );
-        setIsProductInCart(hasProductInCart);
+        productInCart.map((lineitem) => {
+          if (lineitem.productId === productId) {
+            setIsProductInCart(true);
+            setLineItemId(lineitem.id);
+          }
+        });
       }
     };
     void productCheckout();
   }, []);
 
   const addProductToCart = async (): Promise<void> => {
+    const actions = {
+      action: "addLineItem",
+      productId: productId,
+    };
     const cart = await AddingDeletingModifyingItemsInCart(actions);
     setTotalLineItemQuantity(cart.totalLineItemQuantity);
     setIsProductInCart(true);
+    const lastItem = cart.lineItems.slice(-1);
+    setLineItemId(lastItem[0].id);
+  };
+
+  const removeProductFromCart = async (): Promise<void> => {
+    const actions = {
+      action: "removeLineItem",
+      lineItemId: lineItemId,
+    };
+    const cart = await AddingDeletingModifyingItemsInCart(actions);
+    setTotalLineItemQuantity(cart.totalLineItemQuantity);
+    setIsProductInCart(false);
   };
 
   return isProductInCart === false ? (
@@ -52,7 +66,7 @@ export function ButtonAddToCart({
       className="button-remove-from-cart"
       onClick={(event) => {
         event.stopPropagation();
-        //void removeProductFromCart();
+        void removeProductFromCart();
       }}
     >
       REMOVE FROM CART
