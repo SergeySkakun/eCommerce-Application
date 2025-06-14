7b2acc619a9eff329011ac180fe6c21ad3287772
Dmitry <Dololob92@gmail.com>
2025-06-15 02:33:04 +0300

    feat: incorporate and link rs school logo on the about us page

--- src/pages/about-us/about.tsx
+++ src/pages/about-us/about.tsx
@@ -7,6 +7,20 @@ export function About(): ReactElement {
       <main className={styles.aboutPage}>
         <div className={styles.aboutContainer}>
           <p className={styles.title}>About us</p>
+          <div className={styles.aboutContent}>
+            <div className={styles.rsSchool}>
+              <a className={styles.iconRS} href="https://rs.school/">
+                RS School
+              </a>
+              <p className={styles.textRS}>
+                We would like to express our gratitude to RS School and all its
+                staff for giving us the opportunity to try something new and
+                interesting. Thank you for your time. Special thanks to our
+                mentor, who helped and motivated us on this long, difficult, but
+                incredibly exciting journey.
+              </p>
+            </div>
+          </div>
         </div>
       </main>
     </>
